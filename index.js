d51258af4293a3e27b8519c5f986789d5fd61f2b
Usama Qazi <usamyharis@yahoo.com>
2023-03-11 23:10:41 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App setup
+var app = express();
+var server = app.listen(4000, function () {
+    console.log('listening to requests on port 4000');
+});
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+
+    //Handle chat event
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data);
+    })
+
+    //Handle typing event
+    socket.on('typing', function (data) {
+        socket.broadcast.emit('typing', data);
+    })
+});
